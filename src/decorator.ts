bde3772372f8bcb31e60ceed96547503fcf01b58
BenZed <bengaumond@gmail.com>
2023-07-31 11:25:07 -0700

    dependency update

--- src/decorator.ts
+++ src/decorator.ts
@@ -1,18 +1,14 @@
 import { each } from '@benzed/each'
-import { isFunc } from '@benzed/types'
 import { define } from '@benzed/util'
+import { struct, StructStatic } from '@benzed/types'
 
 //// Types ////
 
 interface Trait {}
 
-interface TraitGuard {
-    is(input: unknown): input is Trait
-}
-
 type TraitDefinition =
-    | ((new () => Trait) & TraitGuard)
-    | ((abstract new () => Trait) & TraitGuard)
+    | ((new () => Trait) & StructStatic)
+    | ((abstract new () => Trait) & StructStatic)
 
 //// Decorator ////
 
@@ -23,20 +19,11 @@ function trait<I extends TraitDefinition>(
     Trait: I,
     context: ClassDecoratorContext
 ) {
+    // Inherit @struct trait
+    struct(Trait, context)
+
     // Validation
     context.addInitializer(() => {
-        if (context.kind !== 'class')
-            throw new Error(
-                `${trait.name} may only be used to decorate classes.`
-            )
-
-        if (!isFunc(Trait.is))
-            throw new Error(
-                `${trait.name}s require a static type guard property named ${
-                    'is' satisfies keyof I
-                }`
-            )
-
         const chain = each.prototypeOf(Trait).toArray()
         if (chain.length > 3)
             throw new Error(`${trait.name} may not decorate extended classes.`)
@@ -45,15 +32,6 @@ function trait<I extends TraitDefinition>(
     return define.named(
         Trait.name,
         class extends Trait {
-            /**
-             * @internal
-             * Any object that fulfills the structural contract outlined
-             * by the type guard is considered an instanceof that trait.
-             */
-            static [Symbol.hasInstance](input: unknown) {
-                return this.is(input)
-            }
-
             constructor() {
                 super()
                 throw new Error(
